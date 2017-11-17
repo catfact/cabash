2931f8a4c2389b4414867c74503efe0977f5aa21
ezra buchla <emb@catfact.net>
2017-11-16 22:27:22 -0500

    Update patterns.c
    woops

--- patterns.c
+++ patterns.c
@@ -28,7 +28,7 @@ void pat_fill(bool *p, const int n, pat_t pat) {
     for(int i=0; i<n; ++i) { p[i] = (i%3 == 0); }
     break;
   case PAT_ALT3:
-    for(int i=0; i<n; ++i) { p[i] = (i%1 == 0); }
+    for(int i=0; i<n; ++i) { p[i] = (i%4 == 0); }
     break;
   case PAT_ALT12:
     for(int i=0; i<n; ++i) {
